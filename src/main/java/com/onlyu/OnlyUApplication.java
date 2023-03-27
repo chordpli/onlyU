19154fbdccee0c943ef13103872d84d96c5f293e
Junho Kim <chordplaylist@gmail.com>
2023-03-27 23:15:42 +0900

    build: build project

---
+++ src/main/java/com/onlyu/OnlyUApplication.java
@@ -0,0 +1,13 @@
+package com.onlyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyUApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlyUApplication.class, args);
+  }
+
+}
