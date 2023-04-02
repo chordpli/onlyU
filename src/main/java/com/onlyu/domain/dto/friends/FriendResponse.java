bd82bda66dcefca71044b2fa87c522013116974d
chordpli <chordplaylist@gmail.com>
2023-04-03 01:49:07 +0900

    feat: check chatroom
    1. 채팅방이 만들어져 있다면 메인 화면에서 채팅방 만들기라는 버튼을 없애야 하기 때문에 제작
    2. 채팅방이 만들어져 있는지 확인하는 로직

--- src/main/java/com/onlyu/domain/dto/friends/FriendResponse.java
+++ src/main/java/com/onlyu/domain/dto/friends/FriendResponse.java
@@ -14,11 +14,13 @@ public class FriendResponse {
 
   private Long memberNo;
   private String nickname;
+  private boolean createdChat;
 
-  public static FriendResponse of(Member member) {
+  public static FriendResponse of(Member member, boolean createdChat) {
     return FriendResponse.builder()
         .memberNo(member.getMemberNo())
         .nickname(member.getNickname())
+        .createdChat(createdChat)
         .build();
   }
 }
--- src/main/java/com/onlyu/service/FriendService.java
+++ src/main/java/com/onlyu/service/FriendService.java
@@ -1,14 +1,17 @@
 package com.onlyu.service;
 
 import com.onlyu.domain.dto.friends.FriendResponse;
+import com.onlyu.domain.entity.ChatRoom;
 import com.onlyu.domain.entity.Friends;
 import com.onlyu.domain.entity.Member;
 import com.onlyu.exception.ErrorCode;
 import com.onlyu.exception.OnlyUAppException;
+import com.onlyu.repository.ChatRoomRepository;
 import com.onlyu.repository.FriendRequestRepository;
 import com.onlyu.repository.FriendsRepository;
 import com.onlyu.repository.MemberRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,7 @@ public class FriendService {
 
   private final FriendsRepository friendsRepository;
   private final FriendRequestRepository friendRequestRepository;
+  private final ChatRoomRepository chatRoomRepository;
 
 
   public void addRelation(Member requester, Member receiver) {
@@ -37,7 +41,9 @@ public class FriendService {
     );
 
     List<Member> friends = findFriendsByMember(currentMember);
-    return friends.stream().map(FriendResponse::of).collect(Collectors.toList());
+
+    return friends.stream().map(member -> FriendResponse.of(member, hasChatRoom(member, currentMember)))
+        .collect(Collectors.toList());
   }
 
   List<Member> findFriendsByMember(Member member) {
@@ -48,5 +54,10 @@ public class FriendService {
         .collect(Collectors.toList());
 
     return friends;
+  }
+
+  private boolean hasChatRoom(Member member1, Member member2) {
+    Optional<ChatRoom> createdChat = chatRoomRepository.findByMember1AndMember2(member1, member2);
+    return createdChat.isPresent();
   }
 }
