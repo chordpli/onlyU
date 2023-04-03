63774ae771cbb3a8d7829593b58c00963a99777a
chordpli <chordplaylist@gmail.com>
2023-04-03 23:39:36 +0900

    feat: createdAt 추가

--- src/main/java/com/onlyu/domain/entity/Chat.java
+++ src/main/java/com/onlyu/domain/entity/Chat.java
@@ -3,11 +3,16 @@ package com.onlyu.domain.entity;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Builder
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Chat {
 
 	@Id
@@ -26,12 +32,14 @@ public class Chat {
 	private Long chatNo;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_no", referencedColumnName = "roomNo")
 	private ChatRoom chatRoom;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Member sender;
+	private String sender;
 
 	private String message;
+
+	@CreatedDate
 	private LocalDateTime createdAt;
 
 	public void sendMessage(String message) {
