1a36f9389e79e5529bb25f4df3bdfe5bfe5a3ed3
chordpli <chordplaylist@gmail.com>
2023-04-04 21:51:21 +0900

    refactor: 자료형 변경, 메서드 분리
    1. var를 let과 const로 변경
    2. for반복문 forEach로 변경.

--- src/main/resources/static/js/socket.js
+++ src/main/resources/static/js/socket.js
@@ -1,23 +1,26 @@
 'use strict';
 
-document.write("<script\n" +
-    "  src=\"https://code.jquery.com/jquery-3.6.1.min.js\"\n" +
-    "  integrity=\"sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=\"\n" +
-    "  crossorigin=\"anonymous\"></script>")
+document.write(
+    "<script src=\"https://code.jquery.com/jquery-3.6.1.min.js\" integrity=\"sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=\" crossorigin=\"anonymous\"></script>"
+);
 
 
-var messageForm = document.querySelector('#messageForm');
-var messageInput = document.querySelector('#message');
-var messageArea = document.querySelector('#messageArea');
-var connectingElement = document.querySelector('.connecting');
-
-var stompClient = null;
-var username = null;
-
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const messageArea = document.querySelector('#messageArea');
+const connectingElement = document.querySelector('.connecting');
+const colors = [
+    '#2196F3',
+    '#32c787',
+    '#00BCD4',
+    '#ff5652',
+    '#ffc107',
+    '#ff85af',
+    '#FF9800',
+    '#39bbb0'
 ];
+let stompClient;
+let username;
 
 // roomId 파라미터 가져오기
 const roomNo = document.querySelector('#roomNo').value.trim();
@@ -25,29 +28,24 @@ console.log(roomNo);
 
 function connect() {
     username = document.querySelector('#name').value.trim();
-
     // 연결하고자하는 Socket 의 endPoint
-    var socket = new SockJS('/ws-stomp');
+    const socket = new SockJS('/ws-stomp');
     stompClient = Stomp.over(socket);
-
     stompClient.connect({}, onConnected, onError);
-    console.log("연결 완료");
 }
 
 function onConnected() {
-    // sub 할 url => /sub/chat/room/roomId 로 구독한다
-    stompClient.subscribe('/sub/chat/room/' + roomNo, onMessageReceived);
-    console.log("구독 완료");
+    stompClient.subscribe(`/sub/chat/room/${roomNo}`, onMessageReceived);
     connectingElement.classList.add('hidden');
+    loadChatHistory();
+}
 
+function loadChatHistory() {
     $.ajax({
         type: 'POST',
         url: `/chat/message/${roomNo}`,
         success: function (data) {
-            for (let i = 0; i < data.length; i++) {
-                const chat = data[i];
-                appendChatMessage(chat);
-            }
+            data.forEach(appendChatMessage);
         },
         error: function (err) {
             console.error(err);
@@ -63,8 +61,7 @@ function onError(error) {
 
 // 메시지 전송때는 JSON 형식을 메시지를 전달한다.
 function sendMessage(event) {
-    var messageContent = messageInput.value.trim();
-
+    const messageContent = messageInput.value.trim();
     if (messageContent && stompClient) {
         var chatMessage = {
             "chatRoom": {"roomNo": roomNo},
@@ -78,11 +75,8 @@ function sendMessage(event) {
     event.preventDefault();
 }
 
-// 메시지를 받을 때도 마찬가지로 JSON 타입으로 받으며,
-// 넘어온 JSON 형식의 메시지를 parse 해서 사용한다.
 function onMessageReceived(payload) {
-    //console.log("payload 들어오냐? :"+payload);
-    var chat = JSON.parse(payload.body);
+    const chat = JSON.parse(payload.body);
     appendChatMessage(chat);
 }
 
@@ -93,7 +87,7 @@ function appendChatMessage(chat) {
     const avatarElement = document.createElement('i');
     const avatarText = document.createTextNode(chat.sender[0]);
     avatarElement.appendChild(avatarText);
-    avatarElement.style['background-color'] = getAvatarColor(chat.sender);
+    avatarElement.style.backgroundColor = getAvatarColor(chat.sender);
     messageElement.appendChild(avatarElement);
 
     const usernameElement = document.createElement('span');
